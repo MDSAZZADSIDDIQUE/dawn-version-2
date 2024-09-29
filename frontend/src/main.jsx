cb18e0622291c809d87a52d83e4a14ffa38b22f2
MDSAZZADSIDDIQUE <mdsazzadsiddique0@gmail.com>
2024-09-29 19:44:55 +0600

    Initial commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import Layout from "./components/Layout.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<App />} />
+      </Route>
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
